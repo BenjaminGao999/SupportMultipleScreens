d2a9ab0c29c8c48177f35df4a8895c97b5b38493
littlelittle <1986118324@qq.com>
2017-04-12 15:47:34 +0800

    经验添加

--- supportmultiscreens/src/main/java/com/gaos/supportmultiplescreens/MainActivity.java
+++ supportmultiscreens/src/main/java/com/gaos/supportmultiplescreens/MainActivity.java
@@ -1,12 +1,15 @@
 package com.gaos.supportmultiplescreens;
 
-import android.content.res.Configuration;
+import android.graphics.Point;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.util.TypedValue;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,17 +21,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setTextSize();
-//        getImageSize();
+        setViewWidth();
+        getDpi();
+        compareDrawableWithTextScaleFactor();
     }
-
 
     private void setTextSize() {
         TextView textView = (TextView) findViewById(R.id.tv);
-        float textSize = getResources().getDisplayMetrics().widthPixels * 0.05f;
-        Log.i(TAG, "setTextSize: textsize = " + textSize);
+        float density = getResources().getDisplayMetrics().density;
+        float textSize = 18 * density;
         textView.setTextSize(TypedValue.COMPLEX_UNIT_PX, textSize);
-//        Log.i(TAG, "setTextSize: 18sp = " + getResources().getDimension(R.dimen.textsize) + "px");
+    }
 
+    private void setViewWidth() {
+        View viewMy = findViewById(R.id.view_my);
+        LinearLayout.LayoutParams layoutParams = (LinearLayout.LayoutParams) viewMy.getLayoutParams();
+        layoutParams.width = (int) (getResources().getDisplayMetrics().widthPixels/2.0f);
+//        layoutParams.width = 540;
+        viewMy.setLayoutParams(layoutParams);
+    }
+
+    private void getDpi() {
         DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
         Log.i(TAG, "setTextSize: width = " + displayMetrics.widthPixels + "px");
         Log.i(TAG, "setTextSize: height = " + displayMetrics.heightPixels + "px");
@@ -36,25 +49,30 @@ public class MainActivity extends AppCompatActivity {
         Log.i(TAG, "setTextSize: density = " + displayMetrics.density);
         Log.i(TAG, "setTextSize: xdpi = " + displayMetrics.xdpi);
         Log.i(TAG, "setTextSize: ydpi = " + displayMetrics.ydpi);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            Point outSize = new Point();
+            getWindowManager().getDefaultDisplay().getRealSize(outSize);
+            Log.i(TAG, "getDpi: outSizeX = " + outSize.x + "; outsizeY = " + outSize.y);
+            DisplayMetrics outMetrics = new DisplayMetrics();
+            getWindowManager().getDefaultDisplay().getRealMetrics(outMetrics);
+            Log.i(TAG, "getDpi: realDensity = " + outMetrics.densityDpi);
+            Log.i(TAG, "getDpi: realXdpi = " + outMetrics.xdpi);
+            Log.i(TAG, "getDpi: realYdpi = " + outMetrics.ydpi);
+            Log.i(TAG, "getDpi: realXpx= " + outMetrics.widthPixels);
+            Log.i(TAG, "getDpi: realYpx = " + outMetrics.heightPixels);
+        }
     }
 
-    /**
-     * ①1080*1920 420dpi
-     * 04-07 08:55:16.083 7144-7144/com.gaos.supportmultiplescreens I/MainActivity: getImageSize: height = 257
-     * 04-07 08:55:16.083 7144-7144/com.gaos.supportmultiplescreens I/MainActivity: getImageSize: width = 168
-     * <p>
-     * ②768*1280 xhdpi
-     * 04-07 08:59:59.580 2721-2721/? I/MainActivity: getImageSize: height = 171
-     * 04-07 08:59:59.580 2721-2721/? I/MainActivity: getImageSize: width = 112
-     * <p>
-     * ③ 原始位图尺寸 height = 171 , weight = 112
-     */
-    private void getImageSize() {
-        ImageView imageView = (ImageView) findViewById(R.id.img_sophie);
-        imageView.measure(0, 0);
-        int height = imageView.getMeasuredHeight();
-        int width = imageView.getMeasuredWidth();
-        Log.i(TAG, "getImageSize: height = " + height);
-        Log.i(TAG, "getImageSize: width = " + width);
+    private void compareDrawableWithTextScaleFactor() {
+        ImageView ivSophie = (ImageView) findViewById(R.id.iv_sophie);
+        TextView tvSophie = (TextView) findViewById(R.id.tv_sophie);
+        ivSophie.measure(0, 0);
+        Log.i(TAG, "compareDrawableWithTextScaleFactor: imageview weidth = " + ivSophie.getMeasuredWidth());
+        Log.i(TAG, "compareDrawableWithTextScaleFactor: imageview height = " + ivSophie.getMeasuredHeight());
+        float textSize = tvSophie.getTextSize();
+        Log.i(TAG, "compareDrawableWithTextScaleFactor: textsize = " + textSize);
+        tvSophie.measure(0, 0);
+        Log.i(TAG, "compareDrawableWithTextScaleFactor: textviewHeight = " + tvSophie.getMeasuredHeight());
     }
 }
